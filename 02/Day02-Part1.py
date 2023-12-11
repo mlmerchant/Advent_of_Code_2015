1c3e8d00e21ca8bac4cfb42f5667ebb4102bb777
mlmerchant <matthew.lee.merchant@gmail.com>
2023-12-11 06:55:53 -0600

    Completed day 02 part 1.

--- 02/Day02-Part1.py
+++ 02/Day02-Part1.py
@@ -5,10 +5,25 @@ with open("challenge.txt") as file:
         presents.append(line.strip().split("x"))
         line = file.readline()
 
+# presents = [["2", "3", "4"]]  # Should equal 58
+
 total = 0
 
 for present in presents:
-	present = [int(side) for side in present]
-	total += min(present) # slack
-	for side in present:
-		total += side * 2
+
+    present = [int(side) for side in present]
+
+    length = present[0]
+    width = present[1]
+    height = present[2]
+
+    # 2*l*w + 2*w*h + 2*h*l
+    side1 = 2 * length * width
+    side2 = 2 * width * height
+    side3 = 2 * height * length
+
+    total += min([side1, side2, side3]) / 2
+    total += sum([side1, side2, side3])
+
+# The correct answer is 1586300
+print(int(total))
