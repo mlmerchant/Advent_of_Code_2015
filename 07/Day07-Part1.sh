39b5a0d9760fca37391a656834e6ed7c369ddfd0
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-12-15 07:04:33 -0600

    Update Day07-Part1.sh
    Adding an or function.

--- 07/Day07-Part1.sh
+++ 07/Day07-Part1.sh
@@ -61,3 +61,19 @@ function And() {
     done   
     echo $result
 }
+
+function Or() {
+    local dec1=$1
+    local dec2=$2
+    local length=${#dec1}
+    local result=""
+    local i
+    for ((i=0 ; i < length; i++)); do
+        if [[ ${dec1:$i:1} -eq 1 || ${dec2:$i:1} -eq 1 ]]; then
+            result=${result}1
+        else
+            result=${result}0
+        fi 
+    done   
+    echo $result
+}
