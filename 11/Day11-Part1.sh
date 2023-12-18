e48b53c87e3c1ace6c157f6ce3a46e32ceee5c29
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-12-18 17:58:47 -0600

    Update Day11-Part1.sh
    added function for condition 1

--- 11/Day11-Part1.sh
+++ 11/Day11-Part1.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 function IncrementLetter() {
 echo $1 | tr "a b c d e f g h i j k l m n o p q r s t u v w x y z" "b c d e f g h i j k l m n o p q r s t u v w x y z a"
 [[ $1 == 'z' ]] && return 0
@@ -22,4 +24,23 @@ IncrementString() {
     echo $returnString
 }
 
-IncrementString azzzz
+
+function Condition1() {
+    local marker=1
+    local string=$1
+    local alphabet=( 'aa' 'bb' 'cc' 'dd' 'ee' 'ff' 'gg' 'hh' 'ii' 'jj' 'kk' 'll' 'mm' 'nn' 'oo' 'pp' 'qq' 'rr' 'ss' 'tt' 'uu' 'vv' 'ww' 'xx' 'yy' 'zz' )
+    local i
+    for i in {1..2}; do
+        for pattern in "${alphabet[@]}"; do
+            string=$(echo $string | sed "s/$pattern/$marker/")
+            if (echo $string | grep "$marker" > /dev/null); then
+                if [[ $marker == "1" ]]; then
+                    marker=2
+                else
+                    return 0 # $second pair found
+                fi
+            fi
+        done 
+    done
+    return 1 # failed to find a match
+}
