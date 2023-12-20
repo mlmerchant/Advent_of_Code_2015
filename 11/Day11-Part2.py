ac5a4f60b9db6804d26a1405a611b612cdf72d37
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-12-20 14:27:09 -0600

    Create Day11-Part2.py
    work started on python translation

---
+++ 11/Day11-Part2.py
@@ -0,0 +1,7 @@
+#!/bin/env python3
+
+def IncrementLetter(x):
+    list1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    list2 = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a']
+    return list2[list1.index(x)]
+
