381d01b4321227eb09808e6fa6ba472dbe25fd0c
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-12-20 15:41:15 -0600

    Update Day11-Part2.py
    implemented increment string in python

--- 11/Day11-Part2.py
+++ 11/Day11-Part2.py
@@ -5,3 +5,14 @@ def IncrementLetter(x):
     list2 = ['b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a']
     return list2[list1.index(x)]
 
+def IncrementString(x):
+    returnString=""
+    incrementFlag=True
+    for char in x[::-1]:
+        if incrementFlag:
+            char=IncrementLetter(char)
+            if char != 'a':
+                incrementFlag = False
+        returnString=f"{char}{returnString}"
+    return returnString    
+
