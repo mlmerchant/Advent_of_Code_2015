6cc4d1c5b18661d7a7099e7ef06bd35ad4cace00
Matt Merchant <matthew.lee.merchant@gmail.com>
2023-12-21 17:09:59 -0600

    Update Day15-Part1.py
    Working solution posted.

--- 15/Day15-Part1(WIP).py
+++ 15/Day15-Part1(WIP).py
@@ -1,53 +1,33 @@
-#!/bin/env python3
+# Ingredient properties
+lookup = {
+    "Sprinkles": {"capacity": 5, "durability": -1, "flavor": 0, "texture": 0, "calories": 5},
+    "PeanutButter": {"capacity": -1, "durability": 3, "flavor": 0, "texture": 0, "calories": 1},
+    "Frosting": {"capacity": 0, "durability": -1, "flavor": 4, "texture": 0, "calories": 6},
+    "Sugar": {"capacity": -1, "durability": 0, "flavor": 0, "texture": 2, "calories": 8}
+}
 
-challenge = """Sprinkles capacity 5 durability -1 flavor 0 texture 0 calories 5
-PeanutButter capacity -1 durability 3 flavor 0 texture 0 calories 1
-Frosting capacity 0 durability -1 flavor 4 texture 0 calories 6
-Sugar capacity -1 durability 0 flavor 0 texture 2 calories 8"""
+# Initialize maximum score
+max_score = 0
 
+# Iterate through all possible combinations, ensuring the total quantity is 100
+for a in range(101):  # Sprinkles
+    for b in range(101 - a):  # PeanutButter
+        for c in range(101 - a - b):  # Frosting
+            d = 100 - a - b - c  # Sugar
 
+            # Calculate total for each property, handling negative values
+            value1 = max(0, a * lookup["Sprinkles"]["capacity"] + b * lookup["PeanutButter"]["capacity"] + c * lookup["Frosting"]["capacity"] + d * lookup["Sugar"]["capacity"])
+            value2 = max(0, a * lookup["Sprinkles"]["durability"] + b * lookup["PeanutButter"]["durability"] + c * lookup["Frosting"]["durability"] + d * lookup["Sugar"]["durability"])
+            value3 = max(0, a * lookup["Sprinkles"]["flavor"] + b * lookup["PeanutButter"]["flavor"] + c * lookup["Frosting"]["flavor"] + d * lookup["Sugar"]["flavor"])
+            value4 = max(0, a * lookup["Sprinkles"]["texture"] + b * lookup["PeanutButter"]["texture"] + c * lookup["Frosting"]["texture"] + d * lookup["Sugar"]["texture"])
 
-challenge = challenge.split("\n")
+            # Calculate total score
+            total_score = value1 * value2 * value3 * value4
 
-# create the lookup tables
-lookup = {}
-for line in challenge:
-	contents = line. split(" ")
-	lookup[contents[0]] = {}
-	lookup[contents[0]][contents[1]] = int(contents[2]) # capacity
-	lookup[contents[0]][contents[3]] = int(contents[4]) # durability 
-	lookup[contents[0]][contents[5]] = int(contents[6]) # flavor
-	lookup[contents[0]][contents[7]] = int(contents[8]) # texture
+            # Update maximum score if current score is greater
+            if total_score > max_score:
+                max_score = total_score
 
-max = 0
-for a in range(1, 98): # Sprinkles
-    for b in range(1, 98): # PeanutButter
-        for c in range(1, 98): # Frosting
-            for d in range(1, 98): # Sugar
-                if (a + b + c + d) != 100:
-                    continue
-                Sprinkles = a
-                PeanutButter = b
-                Frosting = c
-                Sugar = d
-                
-                value1 = ( (Sprinkles * lookup["Sprinkles"]["capacity"]) + (PeanutButter * lookup["PeanutButter"]["capacity"]) + (Frosting * lookup["Frosting"]["capacity"])  + (Sugar * lookup["Sugar"]["capacity"]) )
-                value2 = ( (Sprinkles * lookup["Sprinkles"]["durability"]) + (PeanutButter * lookup["PeanutButter"]["durability"]) + (Frosting * lookup["Frosting"]["durability"]) + (Sugar * lookup["Sugar"]["durability"]) )
-                value3 = ( (Sprinkles * lookup["Sprinkles"]["flavor"]) + (PeanutButter * lookup["PeanutButter"]["flavor"]) + (Frosting * lookup["Frosting"]["flavor"]) + (Sugar * lookup["Sugar"]["flavor"])  )
-                value4 = ( (Sprinkles * lookup["Sprinkles"]["texture"]) + (PeanutButter * lookup["PeanutButter"]["texture"]) + (Frosting * lookup["Frosting"]["texture"]) + (Sugar * lookup["Sugar"]["texture"]) )
-                
-                if value1 <= 0 or value2 <= 0 or value3 <= 0 or value4 <= 0:
-                    pass
-                
-                total = value1 * value2 * value3 * value4
-                
-                if total > max:
-                	max = total
-                	
-# 9375000 is too low.  2124702744 is wrong.  40627200 is wrong.
-if max < 9375000:
-    print(f"Answer {max} is too low.")
-else:
-    print(max)
-                
-	
+# Print the maximum score
+# The answer is 13882464
+print(max_score)
